ec41c86076346673c6c79769733ce55e0a742623
jeedddiiii <pendigon.once@gmail.com>
2022-11-25 23:49:25 +0700

    final

---
+++ src/sounds/Sound.java
@@ -0,0 +1,52 @@
+package sounds;
+
+
+import javax.sound.sampled.*;
+import java.io.IOException;
+
+public class Sound {
+
+
+	public static synchronized void playSound(final String strPath) {
+	    new Thread(() -> {
+          try {
+            Clip clp = AudioSystem.getClip();
+
+            AudioInputStream aisStream =
+                    AudioSystem.getAudioInputStream(Sound.class.getResourceAsStream(strPath));
+
+
+            clp.open(aisStream);
+            clp.start();
+          } catch (Exception e) {
+            System.err.println(e.getMessage());
+          }
+        }).start();
+	  }
+	
+	
+
+	public static Clip clipForLoopFactory(String strPath){
+		
+		Clip clp = null;
+		
+		// this line caused the original exceptions
+		
+		try {
+			AudioInputStream aisStream = 
+					  AudioSystem.getAudioInputStream(Sound.class.getResourceAsStream(strPath));
+			clp = AudioSystem.getClip();
+		    clp.open( aisStream );
+				
+		} catch(Exception exp){
+			System.out.println("error");
+		}
+		
+		return clp;
+		
+	}
+	
+	
+
+
+}
